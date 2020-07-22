a21e301f4be5f23a9cd06ddbd20cae997cc2498b
h4ckd0tm3 <marcel@schnideritsch.at>
2020-07-22 08:43:09 +0200

    Fix #2

--- blueprint.py
+++ blueprint.py
@@ -1,11 +1,12 @@
-from flask import request, render_template, Blueprint, abort
+import random
+import string
+
+from flask import request, render_template, Blueprint, abort, redirect, url_for
 from .db_utils import DBUtils
 from CTFd.utils.decorators import admins_only, authed_only
 
 bouncer_bp = Blueprint("bouncer", __name__, template_folder="templates", static_folder="assets")
 
-import random
-import string
 
 def load_bp(plugin_route):
     def invite_generator(size=8, chars=string.ascii_uppercase + string.digits):
@@ -14,7 +15,8 @@ def load_bp(plugin_route):
     @bouncer_bp.route(plugin_route, methods=["GET"])
     @admins_only
     def get_config():
-        return render_template("ctfd_bouncer/config.html", config=DBUtils.get_config(), invites=zip(DBUtils.get_all_invites(), DBUtils.get_all_invite_uses()))
+        return render_template("ctfd_bouncer/config.html", config=DBUtils.get_config(),
+                               invites=zip(DBUtils.get_all_invites(), DBUtils.get_all_invite_uses()))
 
     @bouncer_bp.route(plugin_route, methods=["POST"])
     @admins_only
@@ -23,7 +25,8 @@ def load_bp(plugin_route):
         del config["nonce"]
 
         DBUtils.save_config(config.items())
-        return render_template("ctfd_bouncer/config.html", config=DBUtils.get_config(), invites=zip(DBUtils.get_all_invites(), DBUtils.get_all_invite_uses()))
+        return render_template("ctfd_bouncer/config.html", config=DBUtils.get_config(),
+                               invites=zip(DBUtils.get_all_invites(), DBUtils.get_all_invite_uses()))
 
     @bouncer_bp.route(f"{plugin_route}/<invite_id>", methods=["DELETE"])
     @admins_only
@@ -42,7 +45,6 @@ def load_bp(plugin_route):
         form_data = request.form.to_dict()
         DBUtils.insert_invite(form_data["invite_code"], int(form_data["invite_uses"]))
 
-        return render_template("ctfd_bouncer/config.html", config=DBUtils.get_config(), invites=zip(DBUtils.get_all_invites(), DBUtils.get_all_invite_uses()))
+        return redirect(url_for("bouncer.get_config"))
 
     return bouncer_bp
-
